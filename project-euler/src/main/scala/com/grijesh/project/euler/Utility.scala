d1389afb3ca6669f266f55dac8a3cc962504bb34
grijeshsaini <grijesh.rajoria@gmail.com>
2015-09-21 01:29:47 +0530

    utility changes

--- project-euler/src/main/scala/com/grijesh/project/euler/Utility.scala
+++ project-euler/src/main/scala/com/grijesh/project/euler/Utility.scala
@@ -6,7 +6,7 @@ package com.grijesh.project.euler
 object Utility {
   def time[R](block: => R): R = {
     val t0 = System.currentTimeMillis()
-    val result = block    // call-by-name
+    val result = block
     val t1 = System.currentTimeMillis()
     println("Time taken: " + (t1 - t0) + "ms")
     result
